c513c6a38ea75b288611c8beb5208f765b999514
paolocattaneo <paolo.cattaneo92@gmail.com>
2023-07-14 15:32:35 +0200

    Half course completed

--- main.c
+++ main.c
@@ -12,6 +12,12 @@ void workingWithNumbers();
 void comments();
 void constants();
 void userInput();
+void calculator();
+void madLibs();
+void arrays();
+void functions();
+void ifStatements();
+void betterCalculator();
 
 int main() {
 
@@ -23,6 +29,12 @@ int main() {
     // comments();
     // constants();
     // userInput();
+    // calculator();
+    // madLibs();
+    // arrays();
+    // functions();
+    // ifStatements();
+    betterCalculator();
 
     return 0;
 }
@@ -112,5 +124,100 @@ void userInput() {
     fgets(name, BUFF_SIZE, stdin); //scanf would stop at first whitespace
     printf("Your name is %s", name);
 }
+
+void calculator() {
+    // 8. Build a calculator
+    double a;
+    double b;
+    printf("Enter number A: ");
+    scanf("%lf", &a);
+    printf("Enter number B: ");
+    scanf("%lf", &b);
+    double result = a + b;
+    printf("Result = %f", result);
+}
+
+void madLibs() {
+    const int BUFF_SIZE = 20;
+    char color[BUFF_SIZE];
+    char pluralNoun[BUFF_SIZE];
+    char celebrityF[BUFF_SIZE];
+    char celebrityL[BUFF_SIZE];
+
+    printf("Enter a color: ");
+    scanf("%s", color);
+    printf("Enter a plural noun: ");
+    scanf("%s", pluralNoun);
+    printf("Enter a celebrity name: ");
+    scanf("%s%s", celebrityF, celebrityL);  //support only 1 word
+
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", pluralNoun);
+    printf("I love %s %s\n", celebrityF, celebrityL);
+}
+
+void arrays() {
+    int lostNumbers[] = {4, 8, 15, 16, 23, 42};
+    int index = 2;
+    printf("Before element at index %d is %d\n", index, lostNumbers[index]);
+    lostNumbers[index] = 100;
+    printf("Later element at index %d is %d\n", index, lostNumbers[index]);
+
+    printf("All elements:\n");
+    const int SIZE_OF_INT = sizeof(lostNumbers[0]);
+    int size = sizeof(lostNumbers) / SIZE_OF_INT;
+    for (int i = 0; i < size; i++) {
+        printf("[%d]:\t%d\n", i, lostNumbers[i]);
+    }
+}
+
+double cube(double value);  //must declare before calling it
+void functions() {
+    double value = 2;
+    printf("Cube of %f = %f", value, cube(value));
+}
+
+double cube(double value) {
+    return pow(value, 3);
+}
+
+int max(int a, int b);
+int max2(int a, int b);
+int max3(int a, int b, int c);
+
+void ifStatements() {
+    int a = 10;
+    int b = 20;
+    printf("Max value (v1) between %d and %d is %d\n", a, b, max(a, b));
+    printf("Max value (v2) between %d and %d is %d\n", a, b, max2(a, b));
+
+    int c = 6;
+    printf("In 3 values [%d,%d,%d] max is %d", a, b, c, max3(a, b, c));
+}
+
+int max(int a, int b) {
+    if(a >= b) {
+        return a;
+    }
+    
+    return b;
+}
+
+int max2(int a, int b) {
+    return (a >= b) ? a : b;
+}
+
+int max3(int a, int b, int c) {
+    if(a >= b && a >= c) return a;
+    if(b >= a && b >= c) return b;
+    if(c >= a && c >= b) return c;
+    return -1;
+}
+
+void betterCalculator() {
+    //TODO
+}
+
+
 
 
