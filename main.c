84fd4e9239a60dc97759136e41772de41fcf4452
paolocattaneo <paolo.cattaneo92@gmail.com>
2023-07-17 10:14:11 +0200

    Course complete (no file)

--- main.c
+++ main.c
@@ -18,6 +18,14 @@ void arrays();
 void functions();
 void ifStatements();
 void betterCalculator();
+void switchOp();
+void structs();
+void whileLoop();
+void guessingGame();
+void forLoops();
+void array2D();
+void memory();
+void pointers();
 
 int main() {
 
@@ -34,7 +42,15 @@ int main() {
     // arrays();
     // functions();
     // ifStatements();
-    betterCalculator();
+    // betterCalculator();
+    // switchOp();
+    // structs();
+    // whileLoop();
+    // guessingGame();
+    // forLoops();
+    // array2D();
+    // memory();
+    pointers();
 
     return 0;
 }
@@ -64,6 +80,7 @@ void dataTypes() {
     float y = 2.5;
     char grade = 'A';
     char message[] = "Hello world";
+    printf("%d", integer);
 }
 
 void examplePrintf() {
@@ -215,8 +232,186 @@ int max3(int a, int b, int c) {
 }
 
 void betterCalculator() {
-    //TODO
+    double a;
+    double b;
+    char op;
+
+    printf("Enter number A: ");
+    scanf("%lf", &a);
+    printf("Enter OPERATOR: ");
+    scanf(" %c", &op);
+    printf("Enter number B: ");
+    scanf("%lf", &b);
+
+    double result;
+    if(op == '+') {
+        result = a + b;
+    }
+    else if(op == '-') {
+        result = a - b;
+    }
+    else if(op == '*') {
+        result = a * b;
+    }
+    else if(op == '/' && b != 0) {
+        result = a / b;
+    }
+    else {
+        printf("Operation not allowed");
+        return;
+    }
+
+    printf("Result of %f %c %f = %f", a, op, b, result);
 }
+
+
+void switchOp() {
+    char grade = 'B';
+    
+    const int SIZE = 20;
+    char message[SIZE];
+    switch (grade)
+    {
+    case 'A':
+        strcpy(message, "You did great");
+        break;
+    case 'B':
+        strcpy(message, "You did alright");
+        break;
+    case 'C':
+        strcpy(message, "You did poorly");
+        break;
+    case 'D':
+        strcpy(message, "You did very bad");
+        break;
+    case 'E':
+    case 'F':
+        strcpy(message, "You failed badly");
+        break;
+    default:
+        strcpy(message, "Unknown grade");
+        break;
+    }
+
+    printf(message);
+}
+
+
+struct Student
+{
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
+};
+
+void structs() {
+    struct Student student1;
+    strcpy(student1.name, "Paolo");
+    strcpy(student1.major, "MyMajor");
+    student1.age = 31;
+    student1.gpa = 3.2;
+
+    printf("Student %s gpa: %fs", student1.name, student1.gpa);
+}
+
+void whileLoop() {
+    int index = 1;
+    while(index <= 10) {
+        printf("I am at index:\t%d\n", index);
+        index++;
+    }
+
+    index = 1;
+    do {
+        printf("(do while) I am at index:\t%d\n", index);
+    } while(index++  <= 10);
+}
+
+void guessingGame() {
+    const int SECRET = 5;
+    const int GUESS_LIMIT = 3;
+    int guess;
+    int tries = 0;
+
+    while(guess != SECRET) {
+        if(tries > GUESS_LIMIT) {
+            printf("Too many tries! You can only guess %d times\n", GUESS_LIMIT);
+            break;
+        }
+
+        if(tries > 0) {
+            printf("Wrong! Try again!\n");
+        }
+        
+        printf("Guess the number: ");
+        scanf("%d", &guess);
+        tries++;
+    }
+
+    if(guess == SECRET) {
+        printf("You guessed right after %d tries!\n", tries);
+    }
+    else {
+        printf("You lost!\n");
+    }
+}
+
+void forLoops() {
+    int values[] = { 1, 2, 3, 4, 5};
+    int size = sizeof(values) / sizeof(int);
+    for (int i = 0; i < size; i++)
+    {
+        if(values[i] % 2 == 0) {
+            printf("Value %d is even\n", values[i]);
+        }
+    }
+}
+
+void array2D() {
+    
+    int nums[2][3] = { 
+        { 1, 2, 3},
+        { 4, 5, 6}
+    };
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("[%d][%d]:\t%d\n", i, j, nums[i][j]);
+        }
+    }
+}
+
+void memory() {
+    int age = 30;
+    double gpa = 3.4;
+    char grade = 'A';
+
+    printf("%p\n", &age);
+    printf("%p\n", &gpa);
+    printf("%p\n", &grade);
+}
+
+void pointers() {
+    int age = 30;
+    double gpa = 3.4;
+    char grade = 'A';
+
+    int* pAge = &age;
+    double* pGpa = &gpa;
+    char* pGrade = &grade;
+
+    printf("age's memory address: %p\n", &age);
+}
+
+
+
+
+
+
+
 
 
 
